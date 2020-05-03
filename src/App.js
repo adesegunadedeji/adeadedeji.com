756092fb064f2301aaf16ef0279126be16b64539
Adesegun Adedeji <adesegunadedeji@gmail.com>
2020-05-02 22:58:38 -0700

    testtt

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar';
-import {Route, Switch,BrowserRouter as Router} from 'react-router-dom'
 import About from './components/About/About';
 import Projects from './components/Projects/Projects';
 import styles from './App.module.css';
@@ -8,7 +7,6 @@ import Contact from './components/Contact/Contact';
 
 function App() {
   return (
-    <Router>
     <div className= {styles.container}>
       <Navbar/>
       <About/>
@@ -19,7 +17,6 @@ function App() {
      <p>  Ade Adedeji |{'Copyright © '} |{new Date().getFullYear()}</p>
    </footer>
     </div>
-    </Router>
   );
 }
 
